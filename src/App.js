dc947efeab33df5518af0bbb0d09cb6038809b27
ttahm3d <ttahm3d@gmail.com>
2021-11-02 20:45:35 +0530

    feat: rotuing setup completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import "./App.css";
+import { TransactionsProvider } from "./context/Transactions";
+import Modal from "./components/Modal";
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard/Dashboard"));
+const Transactions = React.lazy(() =>
+	import("./pages/Transactions/Transactions")
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Header />
+				<TransactionsProvider>
+					<Suspense fallback={<div>Loading....</div>}>
+						<Switch>
+							<Route exact path="/dashboard">
+								<Dashboard />
+							</Route>
+							<Route exact path="/transactions">
+								<Transactions />
+							</Route>
+							<Route exact path="/">
+								<Modal />
+							</Route>
+						</Switch>
+					</Suspense>
+				</TransactionsProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
